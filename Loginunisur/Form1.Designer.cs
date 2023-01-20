9d9da00da079cd6f6ea94e330b93e240b29d3ac0
ZazilEscalante <122659112+ZazilEscalante@users.noreply.github.com>
2023-01-19 20:03:12 -0600

    Add files via upload

---
+++ Loginunisur/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Loginunisur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.aviso = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.Button();
+            this.salir = new System.Windows.Forms.Button();
+            this.tbuser = new System.Windows.Forms.TextBox();
+            this.tbcontrasena = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Loginunisur.Properties.Resources.cerradura;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 232);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(381, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LOGIN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(292, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "USUARIO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(292, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "CONTRASENA";
+            // 
+            // aviso
+            // 
+            this.aviso.AutoSize = true;
+            this.aviso.Location = new System.Drawing.Point(22, 324);
+            this.aviso.Name = "aviso";
+            this.aviso.Size = new System.Drawing.Size(10, 13);
+            this.aviso.TabIndex = 4;
+            this.aviso.Text = "-";
+            // 
+            // login
+            // 
+            this.login.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.login.Location = new System.Drawing.Point(298, 278);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.TabIndex = 5;
+            this.login.Text = "ENTRAR";
+            this.login.UseVisualStyleBackColor = false;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // salir
+            // 
+            this.salir.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.salir.Location = new System.Drawing.Point(394, 278);
+            this.salir.Name = "salir";
+            this.salir.Size = new System.Drawing.Size(75, 23);
+            this.salir.TabIndex = 6;
+            this.salir.Text = "SALIR";
+            this.salir.UseVisualStyleBackColor = false;
+            this.salir.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbuser
+            // 
+            this.tbuser.Location = new System.Drawing.Point(444, 113);
+            this.tbuser.Name = "tbuser";
+            this.tbuser.Size = new System.Drawing.Size(100, 20);
+            this.tbuser.TabIndex = 7;
+            this.tbuser.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbcontrasena
+            // 
+            this.tbcontrasena.Location = new System.Drawing.Point(444, 212);
+            this.tbcontrasena.Name = "tbcontrasena";
+            this.tbcontrasena.Size = new System.Drawing.Size(100, 20);
+            this.tbcontrasena.TabIndex = 8;
+            this.tbcontrasena.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(577, 366);
+            this.Controls.Add(this.tbcontrasena);
+            this.Controls.Add(this.tbuser);
+            this.Controls.Add(this.salir);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.aviso);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "LOGINUNISUR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label aviso;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Button salir;
+        private System.Windows.Forms.TextBox tbuser;
+        private System.Windows.Forms.TextBox tbcontrasena;
+    }
+}
+
---
+++ Loginunisur/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loginunisur
+{
+    public partial class Form1 : Form
+    {
+                int intento = 0;
+                int total = 3;
+                string usuario = "zazil";
+                string contra = "7410";
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = Properties.Resources.cerradura2;
+        }
+
+        
+        private void login_Click(object sender, EventArgs e)
+        {
+
+            {
+                if (tbuser.Text == usuario & tbcontrasena.Text == contra)
+                {
+                    MessageBox.Show("Bienvenido");
+                    pictureBox1.Image = Properties.Resources.cerradura;
+                    //pictureBox1.Image = (@"C:\Users\Zazil Laptop\Downloads\cerradura.png)";
+                   
+                }
+                else
+
+                {
+                    intento = intento + 1;
+
+
+                    if (intento >= 3)
+                    { 
+                        login.Enabled = false;
+                    
+                        MessageBox.Show("Demasiados intentos fallidos");
+
+                        aviso.Text = "El programa se ha bloqueado";
+                        pictureBox1.Image = Properties.Resources.cerradura2;                     }
+                    else
+                    {
+                        MessageBox.Show("Usuario o contraseña Incorrectas. Intenta otra vez; tienes " + (total - intento) + " intentos.");
+
+                    }
+
+                }
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
